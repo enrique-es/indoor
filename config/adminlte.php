b862f06caef6aadae98e4091c0ea7e4f240c6d80
Enrique Espinoza <nike.kikk@gmail.com>
2021-02-12 11:22:31 -0600

    configuracion basica del header y logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,8 +14,8 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
-    'title_prefix' => '',
+    'title' => 'Indoor crop',
+    'title_prefix' => 'Indoor-crop | ',
     'title_postfix' => '',
 
     /*
@@ -30,7 +30,7 @@ return [
     |
     */
 
-    'use_ico_only' => false,
+    'use_ico_only' => true,
     'use_full_favicon' => false,
 
     /*
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
-    'logo_img_class' => 'brand-image img-circle elevation-3',
+    'logo' => '<b>Indoor</b>Crop',
+    'logo_img' => 'vendor/adminlte/dist/img/invernadero.png',
+    'logo_img_class' => 'brand-image elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLTE',
+    'logo_img_alt' => 'Indoor-Crop',
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'home')
 
 @section('content_header')
     <h1>Indoor</h1>
